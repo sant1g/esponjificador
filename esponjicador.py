48ffced9f977a2215b8c556d8a2fac36b3e6d334
Santiago Gómez <sag.gomez@gmail.com>
2018-03-04 16:03:11 -0300

    Update esponjicador.py

--- esponjicador.py
+++ esponjicador.py
@@ -2,21 +2,22 @@ import requests
 import argparse
 import random
 import json
-
+ 
 def main():
-    parser = argparse.argumentparser()
+    ''' main '''
+    parser = argparse.ArgumentParser()
     parser.add_argument('message', nargs='+', help='message to put on meme')
     args = parser.parse_args()
     message = ' '.join(''.join(random.choice([c.upper, c.lower])() for c in word) for word in args.message)[:200]
     memeage = generate_meme(message)
     send_to_discord(memeage)
-
+ 
 def generate_meme(message):
     url = 'https://api.imgflip.com/caption_image'
     datastr = {
-        'template_id': '102723630',   #replace template id with your own meme template id from imgflip
-        'username': {user},           # replace with your imgflip user (configparse works well)
-        'password': {password},       # replace with your imgflip pass
+        'template_id': '102156234', # Meme ID
+        'username': '', # imgflip Username
+        'password': '', # imgflip Password
         'boxes[0]': '',
         'boxes[1][text]': message,
         'boxes[2][x]': 10,
@@ -24,20 +25,20 @@ def generate_meme(message):
         'boxes[4][width]': 100,
         'boxes[5][height]': 30,
     }
-    headers = { 'user-agent' : 'mozilla/5.0' }
+    headers = { 'User-Agent' : 'Mozilla/5.0' }
     resp = requests.post(url, data=datastr, headers=headers)
     return json.loads(resp.text)['data']['url']
 
 def send_to_discord(message):
-    url = 'https://discordapp.com/api/webhooks/{id}/{token}' # replace with your discord webhook url
+    url = 'https://discordapp.com/api/webhooks/' # Discord Webhook
     datastr = {'content': f'{message}'}
     headers = {'content-type': 'application/json'}
     req = requests.post(url, json.dumps(datastr), headers=headers)
 
     if req.status_code != 204:
-        print(f'\nreceived {req.status_code} from discord. whoops\n')
-    else: print('meme sent to discord')
+        print(f'\nReceived {req.status_code} from Discord. Whoops\n')
+    else: print('Meme sent to discord')
 
 
 if __name__ == '__main__':
-main()
+    main()
